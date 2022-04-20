4db961c63ec0a0f5e4a113ee6e39a7bcdda12b58
Cosmin Craciun <cosmin.craciun@cwavesoftware.com>
2022-04-20 12:10:26 +0300

    Adding log rotation and timezoned timestamp

--- src/tools.js
+++ src/tools.js
@@ -1,9 +1,10 @@
 var IbusDevices = require('ibus').IbusDevices;
 msgs = require('./messages.js');
 const { createLogger, format, transports } = require("winston");
+require('winston-daily-rotate-file');
 
 
-Array.prototype.hexToAscii = function (array) {
+Array.prototype.hexToAscii = function(array) {
     if (!this)
         return "";
     var result = "";
@@ -11,12 +12,17 @@ Array.prototype.hexToAscii = function (array) {
         var val = parseInt(this[i], 16);
         if (val > 19 && val < 127) {
             result += String.fromCharCode(val);
-        }
-        else {
+        } else {
             result += ".";
         }
     };
     return result;
+}
+
+const timezoned = () => {
+    return new Date().toLocaleString('en-US', {
+        timeZone: 'Europe/Bucharest'
+    });
 }
 
 const log = createLogger({
@@ -25,23 +31,25 @@ const log = createLogger({
             format: format.cli(),
             handleExceptions: true,
         }),
-        new transports.File({
-            filename: "logs/main.log",
-            format: format.combine(format.timestamp(), format.json(), format.prettyPrint()),
+        new transports.DailyRotateFile({
+            filename: "logs/main-%DATE%.log",
+            datePattern: 'YYYY-MM-DDTHH',
+            maxSize: '100m',
+            format: format.combine(format.timestamp({ format: timezoned }), format.json(), format.prettyPrint()),
         })
     ],
     exceptionHandlers: [
-        new transports.File({ 
+        new transports.File({
             filename: "logs/exceptions.log",
-         })
+        })
     ],
     defaultMeta: {
         file: __filename.replace(`${__dirname}/`, ''),
     },
     level: process.env.LOG_LEVEL || 'info'
-  });
+});
 
-String.prototype.asciiToHexString = function (string) {
+String.prototype.asciiToHexString = function(string) {
     if (!this)
         return [];
     var input = this;
@@ -53,80 +61,78 @@ String.prototype.asciiToHexString = function (string) {
     return result.trim();
 }
 
-String.prototype.padLeft = function (length, character) {
+String.prototype.padLeft = function(length, character) {
     try {
         return new Array(length - this.length + 1).join(character || ' ') + this;
-    }
-    catch (err) {
+    } catch (err) {
         return this;
     }
 };
 
-String.prototype.padRight = function (length, character) {
+String.prototype.padRight = function(length, character) {
     try {
         return this + new Array(length - this.length + 1).join(character || ' ');
-    }
-    catch (err) {
+    } catch (err) {
         return this;
     }
 };
 
-Date.prototype.toFormattedString = function () {
+Date.prototype.toFormattedString = function() {
     var result = String(this.getHours()).padLeft(2, '0') + ':' + String(this.getMinutes()).padLeft(2, '0') + ':' + String(this.getSeconds()).padLeft(2, '0') + '.' + String(this.getMilliseconds()).padLeft(3, '0');
     return result;
 };
 
 module.exports = {
-    init: function () {
+    init: function() {
 
     },
-    
-    convertToInt: function (hex) {
+
+    convertToInt: function(hex) {
         var result = [];
         for (var i = 0; i < hex.length; i++) {
             result.push(parseInt(hex[i], 16));
         };
         return result;
     },
-    
-    getPaddedLenBuf: function (text, len) {
+
+    getPaddedLenBuf: function(text, len) {
         var outputTextBuf = new Buffer(len);
         outputTextBuf.fill(0x20);
-        
+
         var textBuf = (new Buffer(text, 'utf-8')).slice(0, len);
-        
+
         // copy to the new padded buffer
         textBuf.copy(outputTextBuf);
-        
+
         return outputTextBuf;
     },
-    
-    compareify: function (src, dst, msg) {
+
+    compareify: function(src, dst, msg) {
         var r = Buffer.concat([new Buffer.from([parseInt(src, 16), parseInt(dst, 16)]), msg]);
-        
+
         //console.log(r, '/', src , '/', dst, '/', msg);
-        
+
         return r;
     },
-    
-    isEq: function (op1, op2) {
+
+    isEq: function(op1, op2) {
         return op1.equals(op2);
     },
-    
+
     //Convert data to hex and compare with message
-    compare: function (data, message) {
+    compare: function(data, message) {
         var b1 = Buffer.concat([new Buffer.from([parseInt(data.src, 16), parseInt(data.dst, 16)]), data.msg]);
         var b2 = Buffer.concat([new Buffer.from([message.src, message.dst]), message.msg]);
         return b1.equals(b2);
     },
-    
+
     //Compare only messages data
-    compareMsg: function (data, message) {
+    compareMsg: function(data, message) {
         return data.msg.equals(message.msg);
     },
-    
+
     //Compare only messages data
-    startsWith: function (data, message) {
+    startsWith: function(data, message) {
         var b1 = data.msg;
         var b2 = message.msg;
         var valid = true;
@@ -135,12 +141,12 @@ module.exports = {
                 return false;
             }
         };
-        
+
         return true;
     },
-    
+
     //Compare only messages data
-    endsWith: function (data, message) {
+    endsWith: function(data, message) {
         var b1 = data.msg;
         var b2 = message.msg;
         var valid = true;
@@ -149,11 +155,11 @@ module.exports = {
                 return false;
             }
         };
-        
+
         return true;
     },
-    
-    intToAscii: function (array) {
+
+    intToAscii: function(array) {
         if (!array)
             return "";
         var result = "";
@@ -161,15 +167,14 @@ module.exports = {
             var val = array[i];
             if (val > 19 && val < 127) {
                 result += String.fromCharCode(val);
-            }
-            else {
+            } else {
                 result += ".";
             }
         };
         return result;
     },
-    
-    asciiToHex: function (string) {
+
+    asciiToHex: function(string) {
         if (!this)
             return [];
         var input = this;
@@ -180,7 +185,7 @@ module.exports = {
         return result;
     },
 
-    logIbusPacket: function (pkt) {
+    logIbusPacket: function(pkt) {
         for (const [key, value] of Object.entries(msgs.messages)) {
             if (this.compareMsg(pkt, value)) {
                 if (parseInt(pkt.src, 16) == value.src && parseInt(pkt.dst, 16) == value.dst) {
@@ -194,6 +199,6 @@ module.exports = {
         log.info(`[IbusPacket] Message:          ${pkt.msg}`);
         log.info(clc.yellow(`[IbusPacket] Message (text):   ${pkt.msg.toString()}\n`));
     },
-    
+
     log
 }
